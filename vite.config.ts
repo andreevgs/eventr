69eef3c550fe31c0921bc2ad960b68bbcbc6a74f
andreevgs <gleb.andreevv12@gmail.com>
2024-12-20 16:31:53 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import components from "unplugin-vue-components/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/eventr/",
   plugins: [
     vueRouter({
       dts: "src/typed-router.d.ts",
